7284cc944a2d1cc3a61112246c53775059c8bb0c
Dhany <dddhany@gmail.com>
2017-07-02 18:17:10 +0800

    signup integration to cognito

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 import NotFound from './containers/NotFound';
 import Home from './containers/Home';
 import Login from './containers/Login';
+import Signup from './containers/Signup';
 import AppliedRoute from './components/AppliedRoute';
 
 export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <Route component={NotFound} />
   </Switch>
 );
