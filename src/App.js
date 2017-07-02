4b970a8ef6901594a8f6ffd64501a21207eef9f4
Dhany <dddhany@gmail.com>
2017-07-02 17:26:16 +0800

    login + logout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { CognitoUserPool, } from 'amazon-cognito-identity-js';
+
 import './App.css';
 import Routes from './Routes';
 import RouteNavItem from './components/RouteNavItem';
+import config from './config.js';
 
 class App extends Component {
   constructor(props) {
@@ -11,27 +14,64 @@ class App extends Component {
 
     this.state = {
       userToken: null,
+      isLoadingUserToken: true,
     };
   }
-
-  updateUserToken = (userToken) => {
+  async componentDidMount() {
+    const currentUser = this.getCurrentUser();
+    if(currentUser===null){
+      this.setState({isLoadingUserToken: false});
+      return;
+    }
+    try{
+      const userToken = await this.getUserToken(currentUser);
+      this.updateUserToken(userToken);
+    } catch(e){
+      alert(e);
+    }
+    this.setState({isLoadingUserToken: false});
+  }
+  updateUserToken = userToken => {
     this.setState({
       userToken: userToken
     });
   }
-  handleNavLink = (event) => {
+  handleNavLink = event => {
     event.preventDefault();
     this.props.history.push(event.currentTarget.getAttribute('href'));
   }
-  handleLogout = (event) => {
+  handleLogout = event => {
+    const currentUser = this.getCurrentUser();
+    if(currentUser!== null){
+      currentUser.signOut();
+    }
     this.updateUserToken(null);
+  }
+  getCurrentUser(){
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID,
+    });
+    return userPool.getCurrentUser();
+  }
+  getUserToken(currentUser){
+    return new Promise((resolve, reject)=>{
+      currentUser.getSession(function(err, session){
+        if(err){
+          reject(err);
+          return;
+        }
+        resolve(session.getIdToken().getJwtToken());
+      })
+    });
   }
   render() {
     const childProps = {
       userToken: this.state.userToken,
       updateUserToken: this.updateUserToken,
     };
-    return (
+    return ! this.state.isLoadingUserToken &&
+    (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
